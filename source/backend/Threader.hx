ea38dfd7f2de201021e13a94c915e3b8fe9f12fb
Noobz4Life <thenoobattf2@gmail.com>
2023-09-10 17:25:18 -0400

    thread pooling

--- source/backend/Threader.hx
+++ source/backend/Threader.hx
@@ -1,27 +1,37 @@
 package backend;
 
+import sys.thread.ElasticThreadPool;
 import sys.thread.Lock;
 import sys.thread.Thread;
 
 class Threader {
+    private static final threadPool:ElasticThreadPool = new ElasticThreadPool(4,30);
+
     private static var threads:Int = 0;
     private static var lock:Lock = new Lock();
 
-    public static function create(job:() -> Void):Thread {
+    public static function create(job:() -> Void,?important:Bool = false):Void {
         #if (target.threaded)
         if(ClientPrefs.data.multiThreading) {
-            var thread = Thread.create(function() {
-                job();
-                lock.release();
-            });
-            threads++;
-            return thread;
+            if(!important) {
+                threadPool.run(function() {
+                    job();
+                    lock.release();
+                });
+                threads++;
+            } else {
+                Thread.create(function() {
+                    job();
+                    lock.release();
+                });
+                threads++;
+            }
         } else #end job();
-        return null;
     }
 
     public static function wait() {
         while(threads > 0) {
+            trace("Waiting for thread "+threads);
             lock.wait();
             threads--;
         }
