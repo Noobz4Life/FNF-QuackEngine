00a6c3e08606fa12810398ca68f7e6b735d8917a
Noobz4Life <thenoobattf2@gmail.com>
2023-09-11 13:14:23 -0400

    main menu music is bugged sometimes

--- source/flixel/sound/FlxSound.hx
+++ source/flixel/sound/FlxSound.hx
@@ -473,23 +473,20 @@ class FlxSound extends FlxBasic
 	 */
 	public function play(ForceRestart:Bool = false, StartTime:Float = 0.0, ?EndTime:Float):FlxSound
 	{
-		backend.Threader.create(function() {
-			if (!exists)
-				return this;
-
-			if (ForceRestart)
-				cleanup(false, true);
-			else if (playing) // Already playing sound
-				return this;
-
-			if (_paused)
-				resume();
-			else
-				startSound(StartTime);
-
-			endTime = EndTime;
+		if (!exists)
 			return this;
-		});
+
+		if (ForceRestart)
+			cleanup(false, true);
+		else if (playing) // Already playing sound
+			return this;
+
+		if (_paused)
+			resume();
+		else
+			startSound(StartTime);
+
+		endTime = EndTime;
 		return this;
 	}
 
