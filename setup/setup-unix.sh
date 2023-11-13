0047c5f2240b7f1533b7a3c626a436585c1b4da4
Noobz4Life <thenoobattf2@gmail.com>
2023-11-13 01:35:39 -0500

    add flxanimate to setup stuffs

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -13,3 +13,4 @@ haxelib install SScript
 haxelib install hxCodec
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate.git dev
