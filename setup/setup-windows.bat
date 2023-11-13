b304e76679168b6667ef4cb04fe4e534cf09900d
Noobz4Life <thenoobattf2@gmail.com>
2023-11-13 01:37:30 -0500

    lock versions in setup scripts

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,14 +3,14 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install tjson
-haxelib install flixel-addons
-haxelib install flixel-tools
+haxelib install lime 8.0.2
+haxelib install openfl 9.2.2
+haxelib install flixel 5.3.1
+haxelib install tjson 1.4.0
+haxelib install flixel-addons 3.2.0
+haxelib install flixel-tools 1.5.1
 haxelib install SScript
-haxelib install hxCodec
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate.git dev
