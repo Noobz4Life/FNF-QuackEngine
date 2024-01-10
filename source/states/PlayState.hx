4389b5cea0da85328d8ae86aaffa15030c21f2f1
Noobz4Life <thenoobattf2@gmail.com>
2024-01-10 02:00:17 -0500

    health tweening fixes

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1776,6 +1776,8 @@ class PlayState extends MusicBeatState
 		FlxG.watch.addQuick("beatShit", curBeat);
 		FlxG.watch.addQuick("stepShit", curStep);
 
+		FlxG.watch.addQuick("health", health);
+
 		// RESET = Quick Game Over Screen
 		if (!ClientPrefs.data.noReset && controls.RESET && canReset && !inCutscene && startedCountdown && !endingSong)
 		{
@@ -1911,10 +1913,12 @@ class PlayState extends MusicBeatState
 		// update health bar
 		health = value;
 		var newPercent:Null<Float> = FlxMath.remapToRange(FlxMath.bound(healthBar.valueFunction(), healthBar.bounds.min, healthBar.bounds.max), healthBar.bounds.min, healthBar.bounds.max, 0, 100);
-		healthBar.percent = (newPercent != null ? newPercent : 0);
+		if(!ClientPrefs.data.healthBarTweening) {healthBar.percent = (newPercent != null ? newPercent : 0);}
 
-		iconP1.animation.curAnim.curFrame = (healthBar.percent < 20) ? 1 : 0; //If health is under 20%, change player icon to frame 1 (losing icon), otherwise, frame 0 (normal)
-		iconP2.animation.curAnim.curFrame = (healthBar.percent > 80) ? 1 : 0; //If health is over 80%, change opponent icon to frame 1 (losing icon), otherwise, frame 0 (normal)
+		var healthPercent = (newPercent != null ? newPercent : health*50);
+
+		iconP1.animation.curAnim.curFrame = (healthPercent < 20) ? 1 : 0; //If health is under 20%, change player icon to frame 1 (losing icon), otherwise, frame 0 (normal)
+		iconP2.animation.curAnim.curFrame = (healthPercent > 80) ? 1 : 0; //If health is over 80%, change opponent icon to frame 1 (losing icon), otherwise, frame 0 (normal)
 		return health;
 	}
 
